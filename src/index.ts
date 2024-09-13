7c690de61cd122f777fd02332140bc3933c85ebb
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-09-13 08:18:38 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * If possible, put your minimal reproduction in this file, but feel free to
+ * create other files elsewhere in the project if necessary.
+ *
+ * Once you have a minimal reproduction, commit it and push it to a public
+ * GitHub repo.
+ */
+
+const s = "foo";
+// quickfix should be suggested here but it is not
+const v = s + "bar";
